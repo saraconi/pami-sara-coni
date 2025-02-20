abb104fd32d50f8db95b8bbec8d2f491901f9bf0
Sara Coni <conisara96@gmail.com>
2025-02-20 08:28:19 -0300

    Aula sobre os tipos de variaveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,23 @@
+var numero =2;
+let texto ="Hello";
+const oQueEisso = "?";
+// o ponto e virgula é opcipnal 
+
+console.log ('Tipo var:', numero);
+// concatenar com virgula me garante um espaço 
+
+numero = 13;
+
+console.log (texto + "o número agora é: " + numero)
+console.log (texto, "o número agora é: ", numero)
+ console.log (`${texto} o numero agora é ${numero}`)
+
+
+
+texto = 69;
+
+console.log (texto + numero);
+// concatenar com + dois valores numeros vai acabar somando 
+
+
+
